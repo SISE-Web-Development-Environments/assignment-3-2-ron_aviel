d5f3efd9605893994ab06a5adaaca314cb6597f1
Shakutaigit <ron.shakutai@gmail.com>
2020-06-01 17:45:23 +0300

    asdasd

--- Codes/routes/Recipes.js
+++ Codes/routes/Recipes.js
@@ -83,51 +83,6 @@ router.get('/getRecipeMaking/:id',(req, res,next) => {
 });
 
 
-function getRecipeInfo(id) {
-  return axios.get(`${api_domain}/${id}/information`, {
-    params: {
-      includeNutrition: false,
-      apiKey: process.env.spooncular_apiKey
-    }
-  });
-}
 
 
-function getRecipeInMaking(id){
-  return axios.get(`${api_domain}/${id}/analyzedInstructions`, {
-    params: {
-      stepBreakdown: false,
-      apiKey: process.env.spooncular_apiKey
-    }
-  });
-}
-
-function getDisplay(recipe,favorites,lastSeen){
-  var recipeToReturn=new Object();
-  recipeToReturn.id=recipe.data.id;
-  recipeToReturn.photo=recipe.data.image;
-  recipeToReturn.title=recipe.data.title;
-  recipeToReturn.readyInMinutes=recipe.data.readyInMinutes;
-  recipeToReturn.aggregateLikes=recipe.data.aggregateLikes;
-  recipeToReturn.vegan=recipe.data.vegan;
-  recipeToReturn.glutenFree=recipe.data.glutenFree;
-  if(favorites)
-    recipeToReturn.favorites="true";
-  else
-    recipeToReturn.favorites="false";
-  if(lastSeen)
-    recipeToReturn.lastSeen="true";
-  else
-    recipeToReturn.lastSeen="false";
-  return recipeToReturn;
-}
-
-function getFullDisplay(recipe){
-  var recipeToReturn=getDisplay(recipe);
-  recipeToReturn.servings=recipe.data.servings;
-  recipeToReturn.analyzedInstructions=recipe.data.analyzedInstructions;
-  recipeToReturn.extendedIngredients=recipe.data.extendedIngredients;
-  return recipeToReturn
-}
-module.exports = {router,getRecipeInfo,getDisplay,getRecipeInMaking,getFullDisplay};
 module.exports=router;
--- Codes/routes/recipe.js
+++ Codes/routes/recipe.js
@@ -1,35 +1,57 @@
 
 var express = require("express");
-var router = express.Router();
 const DButils = require("./sqlconnect");
 const bcrypt = require("bcrypt");
-
+const axios = require("axios");
+const api_domain = "https://api.spoonacular.com/recipes";
 
 //Recipe REST requsts
 
-router.get('/:id', (req, res) => {
-	res.status(200).send("Hello World");
-});
+function getRecipeInfo(id) {
+	return axios.get(`${api_domain}/${id}/information`, {
+	  params: {
+		includeNutrition: false,
+		apiKey: process.env.spooncular_apiKey
+	  }
+	});
+  }
+  
+  
+  function getRecipeInMaking(id){
+	return axios.get(`${api_domain}/${id}/analyzedInstructions`, {
+	  params: {
+		stepBreakdown: false,
+		apiKey: process.env.spooncular_apiKey
+	  }
+	});
+  }
+  
+  function getDisplay(recipe,favorites,lastSeen){
+	var recipeToReturn=new Object();
+	recipeToReturn.id=recipe.data.id;
+	recipeToReturn.photo=recipe.data.image;
+	recipeToReturn.title=recipe.data.title;
+	recipeToReturn.readyInMinutes=recipe.data.readyInMinutes;
+	recipeToReturn.aggregateLikes=recipe.data.aggregateLikes;
+	recipeToReturn.vegan=recipe.data.vegan;
+	recipeToReturn.glutenFree=recipe.data.glutenFree;
+	if(favorites)
+	  recipeToReturn.favorites="true";
+	else
+	  recipeToReturn.favorites="false";
+	if(lastSeen)
+	  recipeToReturn.lastSeen="true";
+	else
+	  recipeToReturn.lastSeen="false";
+	return recipeToReturn;
+  }
+  
+  function getFullDisplay(recipe){
+	var recipeToReturn=getDisplay(recipe);
+	recipeToReturn.servings=recipe.data.servings;
+	recipeToReturn.analyzedInstructions=recipe.data.analyzedInstructions;
+	recipeToReturn.extendedIngredients=recipe.data.extendedIngredients;
+	return recipeToReturn
+  }
+  module.exports = {getRecipeInfo,getDisplay,getRecipeInMaking,getFullDisplay};
 
-router.get('/:name', (req, res) => {
-	res.status(200).send("Hello World");
-});
-
-router.get('/getRandomRecipes', (req, res) => {
-	res.status(200).send("Hello World");
-});
-
-router.get('/getFamilyRecipes', (req, res) => {
-	res.status(200).send("Hello World");
-});
-
-router.get('/getCreatedByUser/:userID', (req, res) => {
-	res.status(200).send("Hello World");
-});
-
-router.post('/', (req, res) => {
-	res.status(200).send("Hello World");
-});
-
-
-module.exports = router;
