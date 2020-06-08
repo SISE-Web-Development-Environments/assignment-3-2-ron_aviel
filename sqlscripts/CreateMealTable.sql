97f80d7449f0c0515e9d075a9ff4c64216305c7d
Shakutaigit <ron.shakutai@gmail.com>
2020-06-08 16:10:26 +0300

    after meal

--- sqlscripts/CreateMealTable.sql
+++ sqlscripts/CreateMealTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[meals](
     [meal_id] [UNIQUEIDENTIFIER] NOT NULL default NEWID(),
     [user_id] [UNIQUEIDENTIFIER] FOREIGN KEY REFERENCES users(user_id) NOT NULL,
-    [meal_array] [varchar(8000)]
-    
+    [meal_array] [varchar](8000),
+    [process_array][varchar](8000)
 )
