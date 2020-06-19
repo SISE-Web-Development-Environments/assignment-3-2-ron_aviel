a875b4f0d654e43ccc0790dad5359751bb6f4fb1
Shakutaigit <ron.shakutai@gmail.com>
2020-06-19 15:03:21 +0300

    asdasd

--- Codes/main.js
+++ Codes/main.js
@@ -21,13 +21,13 @@ app.use(
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 
-// const corsConfig = {
-//   origin: true,
-//   credentials: true
-// };
+const corsConfig = {
+  origin: true,
+  credentials: true
+};
 
-// app.use(cors(corsConfig));
-// app.options("*", cors(corsConfig));
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 
 var port = process.env.PORT || "5000";
 //#endregion
